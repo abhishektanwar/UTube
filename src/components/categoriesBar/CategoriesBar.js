fc6239dc9515b4798c6b574c458f01a7e59cc07d
=abhishektanwar <abhishektanwar084@gmail.com>
2021-04-26 17:25:26 +0530

    added categories bar

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './categoriesBar.css'
 const CategoriesBar = () => {
+	const keywords = [
+		'All',
+		'React.js',
+		'Angular js',
+		'React Native',
+		'Redux',
+		'Music',
+		'Django',
+		'Javascript',
+		'Coding',
+		'Adventure',
+		'Songs',
+		'Football',
+		'Cricket',
+		'Algorithms',
+		'DSA'
+	]
+
+	const [activeElement,setActiveElement] = useState('All')
+
 	return (
-		<div>
-			categories bar
+		<div className="categoriesBar">
+			{
+				keywords.map((value,i) => (<span 
+						onClick={() => {setActiveElement(value)}}
+						className={activeElement === value ? 'pro active' : ''}
+						key={i}>{value}</span>))
+			}
+
 		</div>
 	)
 }
